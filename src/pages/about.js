b58472917ed5e8442e578e7b7cb40e6b09eed7a9
Alex Bryant <bryantal@berea.edu>
2022-08-11 00:10:17 -0400

    Added main index page, about page, and components folder

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About the Blog</title>
+      <h1>About the Blog</h1>
+      <Link to="/">Home</Link>
+      <p>If you're reading this, that means you're cool.</p>
+    </main>
+  )
+}
+
+export default AboutPage
