427baedaf7f702801738ff3aaa10fb342e27da20
Alex Bryant <bryantal@berea.edu>
2022-08-11 18:39:49 -0400

    added content to the blog mdx files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,32 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const MainBlogPage = () => {
+const MainBlogPage = ({ data }) => {
   return (
     <Layout pageTitle="The Cool Blog Posts">
       <p>Click on a blog post to have a read!</p>
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+          {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default MainBlogPage
