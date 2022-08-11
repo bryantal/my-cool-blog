05f19f89f7488bf3127353e0d87f9c6d80ac69b2
Alex Bryant <bryantal@berea.edu>
2022-08-10 21:34:53 -0400

    messing around with a query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: 'The Cool Blog',
+    description: 'This blog is for cool kids only'
   },
   plugins: [],
 }
